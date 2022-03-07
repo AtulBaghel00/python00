597f70cb809e2259b597db549a3707517ee6d3f1
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-07 20:05:23 +0530

    program to create a file

---
+++ filename.py
@@ -0,0 +1,17 @@
+f=open('filename.txt','rb')
+#out=f.read()
+out = f.seek(50,0)
+print(out)
+
+
+
+
+
+
+
+#out= f.readline()
+#out2=f.readline(4)
+#out3= f.readlines()
+#print(out)
+#print(out2)
+#print(out3)
