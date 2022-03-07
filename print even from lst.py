71b70341adc6366ac4be2f5638876c3d199c5992
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-07 20:08:42 +0530

    print even number from list

---
+++ print even from lst.py
@@ -0,0 +1,76 @@
+##lst=[1,2,3,4,5,6,7,8,12,46,45]
+##lst2=[]
+##for i in lst:
+##    if i%2==0:
+##       lst2.append(i)
+##print(lst2)
+        
+##lst=[1,2,3,3,4,4,5]
+##lst2=[]
+##for i in lst:
+##    if lst.count(i)>1:
+##       i
+##       
+##    lst.append(i)
+##print(lst)
+        
+        
+    
+       
+##str="harryisagoodboy"
+##lst=list(str)
+##for i in lst:
+##    print(i,lst.count(i))
+
+##str="ha234p pynew ''@##^&*year"
+##for i in str:
+##    if (i>='a' and i<='z')or (i>='A' and i<='Z'):
+##        print(i,end="")
+
+##str="happybirdayacdear"
+##for i in str:
+##    str = str.replace("b",'')
+##    str = str.replace("ac",'')
+##print(str)
+
+str="harryyggrreatbboyy"
+lst2=str.split()
+for i in lst2:
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
