9e5a6201e49043b41456654a53ef6f1ac1862ce5
Atul Baghel <92114450+AtulBaghel00@users.noreply.github.com>
2022-03-07 20:09:54 +0530

    pattern

---
+++ pattern.py
@@ -0,0 +1,108 @@
+##row=5
+##for i in range(1,row+1):
+##    for j in range(i):
+##        #print("*",end=" ")
+##        print(j+1,end=" ")
+##    print()
+
+# pattern 9 little mistkake
+##row= 5
+##for i in range(1,row+1):
+##    for j in range(row-i):
+##        print(" ",end=" ")
+##     
+##    for j in range(i):
+##        print(" ",end="")
+##        print("*",end="")
+##    print()
+##    
+##
+##    for k in range((2*row)-i):
+##        print(" ",end=" ")
+##     
+##    for k in range(i):
+##        print(" ",end="")
+##        print("*",end="")
+##    print()
+
+#pattern 2
+
+##row=5
+##for i in range(1,row+1):
+##    for j in range(row+1-i):
+##        print("*",end=" ")
+##    print()
+
+
+### pattern 3
+##
+##row=5
+##for i in range(1,row+1):
+##    for j in range(row-i):
+##        print(" ", end="")
+##    for j in range(i):
+##        print("*",end="")
+##    print()
+
+
+# pattern 4
+
+##row=5
+##for i in range(1,row+1):
+##    for j in range(i-1):
+##        print(" ",end="")
+##    for j in range(row-i+1):
+##        print("*",end="")
+##    print() 
+
+
+
+#pattern 8
+
+row=5
+for i in range(1,row+1):
+    for j in range(row-i):
+        print(" ",end="")
+    for j in range(2*i-1):
+        print("*",end="")
+    
+        for j in range(i):
+            print(" ",end="")
+        for j in range(2*row-i):
+            print("*",end="")
+        print()
+    
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
